2cdddb488820a7f5a1788b1796f7552fd4e07229
tct123 <42028373+tct123@users.noreply.github.com>
2023-01-14 21:20:28 +0100

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -1,12 +1,18 @@
 import requests
+import json
 
 def getQuotes():
     category = 'happiness'
-    api_url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(category)
-    api_key =""
+    api_url = f'https://api.api-ninjas.com/v1/quotes?category={category}'
+    api_key ="As3XyZ3Xl3IEUqpyUHpa6A==phjO5S4DqLxtFeqk"
     response = requests.get(api_url, headers={'X-Api-Key': api_key})
     if response.status_code == requests.codes.ok:
-        print(response.text)
+        data = response.json()
+        quote = data[0]["quote"]
+        author = data[0]["author"]
+        content = f"{quote}\n\n{author}"
+        print(content)
+        return content
     else:
         print("Error:", response.status_code, response.text)
-        
+#getQuotes()
