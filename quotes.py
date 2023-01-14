f4cfbb38710b8de14ad0492c40df2ad08d5f22d5
tct123 <42028373+tct123@users.noreply.github.com>
2023-01-14 20:59:53 +0100

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -1,1 +1,11 @@
 import requests
+
+def getQuotes():
+    category = 'happiness'
+    api_url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(category)
+    api_key =""
+    response = requests.get(api_url, headers={'X-Api-Key': api_key})
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print("Error:", response.status_code, response.text)
