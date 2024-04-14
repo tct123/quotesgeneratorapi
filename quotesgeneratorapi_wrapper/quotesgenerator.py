ef008c0f134e833ce52711b662f5c3a27aeb6261
tct123 <42028373+tct123@users.noreply.github.com>
2024-04-14 23:01:08 +0200

    .

--- quotesgeneratorapi_wrapper/quotesgenerator.py
+++ quotesgeneratorapi_wrapper/quotesgenerator.py
@@ -3,8 +3,8 @@ import json
 
 
 def getQuotes(category, api_key):
-    api_url = f'https://api.api-ninjas.com/v1/quotes?category={category}'
-    response = requests.get(api_url, headers={'X-Api-Key': api_key})
+    api_url = f"https://api.api-ninjas.com/v1/quotes?category={category}"
+    response = requests.get(api_url, headers={"X-Api-Key": api_key})
     if response.status_code == requests.codes.ok:
         data = response.json()
         quote = data[0]["quote"]
@@ -13,11 +13,3 @@ def getQuotes(category, api_key):
         return content
     else:
         print("Error:", response.status_code, response.text)
-
-
-if __name__ == "__main__":
-    try:
-        api_key = input("Input API-key: ")
-        getQuotes(category="happiness", api_key=api_key)
-    except:
-        pass
